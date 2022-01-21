4e5f4ff4eede7bbddbede08baf4bdf102ef81985
ysandeepkumarreddy <55143627+ysandeepkumarreddy@users.noreply.github.com>
2022-01-21 14:25:18 +0530

    weather

---
+++ js/app.js
@@ -0,0 +1,43 @@
+function getWeather() {
+  let temperature = document.getElementById("temperature");
+  let description = document.getElementById("description");
+  let location = document.getElementById("location");
+
+  let api = "https://api.openweathermap.org/data/2.5/weather";
+  let apiKey = "031448f11e54d1a3ab064ad76005c298";
+
+  location.innerHTML = "Locating...";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  function success(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+
+    let url =
+      api +
+      "?lat=" +
+      latitude +
+      "&lon=" +
+      longitude +
+      "&appid=" +
+      apiKey +
+      "&units=metric";
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        let temp = data.main.temp;
+        temperature.innerHTML = temp + "° C";
+        location.innerHTML = data.name;
+        description.innerHTML = data.weather[0].main;
+      });
+  }
+
+  function error() {
+    location.innerHTML = "Unable to retrieve your location";
+  }
+}
+
+getWeather();
